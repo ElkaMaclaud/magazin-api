bea8869790a9d2ce7ecf6b0b357760462af63e1e
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-11-03 21:25:27 +0300

    fix: permissions for all websocket connections

--- services/socket.js
+++ services/socket.js
@@ -8,7 +8,7 @@ dotenv.config();
 export const createSocketServer = (httpServer) => {
     const io = new Server(httpServer, {
         cors: {
-            origin: "https://magazin-ruby.vercel.app", // http://localhost:3001
+            origin: "*", // "https://magazin-ruby.vercel.app" "http://localhost:3001"
             methods: ["GET", "POST"],
             credentials: true 
         }
